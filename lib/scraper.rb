76382940302f0b2539da2094570aa2edcdb08a82
Dario Carlino <baires82@gmail.com>
2020-09-14 16:02:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
-doc = Nokogiri::HTML(html)
+# html = open("https://flatironschool.com/")
+# doc = Nokogiri::HTML(html)
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
